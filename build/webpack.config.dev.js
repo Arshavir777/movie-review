08b7c9532dfdb462c7e0b7ea1abeccfb044a25d0
= <=>
2019-09-20 20:14:26 +0300

    F1

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -1,29 +1,52 @@
-'use strict'
-
+var path = require('path')
 const { VueLoaderPlugin } = require('vue-loader')
-var path = require('path');
+var HrmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
-  entry: [
-    './src/app.js'
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        use: 'vue-loader'
-      },
-      {
-        test: /\.(c|sa)ss$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
+    devServer: {
+        contentBase: path.join('public'),
+        publicPath: '/',
+        compress: true,
+        port: process.env.PORT || 8080
+    },
+    mode: 'none',
+    entry: [ './src/app.js' ],
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.s(c|a)ss$/,
+                use: [
+                  'vue-style-loader',
+                  'css-loader',
+                  {
+                    loader: 'sass-loader',
+                    options: {
+                      implementation: require('sass'),
+                      fiber: require('fibers'),
+                      indentedSyntax: true // optional
+                    }
+                  }
+                ]
+              },
+              {
+                test: /\.css$/,
+                use: [ 'vue-style-loader', 'css-loader']
+              }
         ]
-      }
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new HrmlWebpackPlugin({
+            template: "./public/index.html",
+            inject: 'body'
+        })
     ]
-  },
-  plugins: [
-    new VueLoaderPlugin()
-  ]
 }
